d3d75e63d1b0a6cb3e675fc5548c984935955100
Pulipuli Chen <blog@pulipuli.info>
2019-10-04 16:30:21 +0800

    1004 加上了cors server

---
+++ cors/index.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var serveStatic = require('serve-static')
+var path = require('path')
+ 
+var app = express()
+ 
+app.use(serveStatic(path.resolve(__dirname, 'public'), { 'index': ['index.html'] }))
+app.listen(3000)
