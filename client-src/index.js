540293dc2a7544738e791de2b0be16d73eb23f54
Pulipuli Chen <blog@pulipuli.info>
2019-10-04 16:55:48 +0800

    1004 把webpack的環境也整理進去了

---
+++ client-src/index.js
@@ -0,0 +1,14 @@
+let $ = require('jquery')
+
+//$('body').append('<h1>AAA</h1>')
+
+$('#addUser').click(() => {
+  console.log('addUser')
+})
+
+
+let list = $('#usersList')
+
+$('#loadUsers').click(() => {
+  console.log('loadUser')
+})
